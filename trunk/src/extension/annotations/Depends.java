bb4c892b98e131c78eaf06f17d86adb0d9a070da
Lea Haensenberger <baernerin@gmail.com>
2007-09-18 11:09:38 +0000

    new Annotation Depends
    git-svn-id: file:///Users/akuhn/Checkout/jeg/sf@36 54883e8d-cf1d-0410-83d6-f114b9419f37

---
+++ trunk/src/extension/annotations/Depends.java
@@ -0,0 +1,23 @@
+package extension.annotations;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ */
+
+/**
+ * @author Lea Hänsenberger (lhaensenberger at students.unibe.ch)
+ */
+
+@Retention( RetentionPolicy.RUNTIME )
+@Target( ElementType.METHOD )
+@Documented
+public @interface Depends {
+
+	String value();
+
+}
