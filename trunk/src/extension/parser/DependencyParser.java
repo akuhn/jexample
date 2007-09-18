2376f64b83aa473e2009d86fdd76f8ac11e2480e
Lea Haensenberger <baernerin@gmail.com>
2007-09-18 11:54:35 +0000

    started with a dependency parser, a parser which extracts method names and parameters and returns a list with the Method Objects
    git-svn-id: file:///Users/akuhn/Checkout/jeg/sf@38 54883e8d-cf1d-0410-83d6-f114b9419f37

---
+++ trunk/src/extension/parser/DependencyParser.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package extension.parser;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import extension.MyTestClass;
+
+/**
+ * @author Lea Haensenberger (lhaensenberger at students.unibe.ch)
+ */
+public class DependencyParser {
+
+	private final String annotationValue;
+
+	private final MyTestClass testClass;
+
+	private List<Method> dependencies;
+
+	public DependencyParser( String value, MyTestClass myTestClass ) {
+		this.annotationValue = value;
+		this.testClass = myTestClass;
+
+	}
+
+	public List<Method> getDependencies() throws ClassNotFoundException, SecurityException, NoSuchMethodException {
+		this.dependencies = new ArrayList<Method>();
+
+		String[] methodNames = this.getMethodNames();
+		for ( String dependency : methodNames ) {
+			String[] parameters = this.getParameters( dependency );
+			this.addDependency( dependency, parameters );
+		}
+
+		return dependencies;
+	}
+
+	private void addDependency( String dependency, String[] parameters ) throws ClassNotFoundException, SecurityException, NoSuchMethodException {
+		Method dep = this.testClass.getJavaClass().getMethod( this.extractName(dependency), this.getParameterClasses( parameters ) );
+		if ( !this.dependencies.contains( dep ) ) {
+			this.dependencies.add( dep );
+		}
+	}
+
+	private String extractName( String dependency ) {
+		int index;
+		if ( ( index = dependency.indexOf( "(" ) ) > -1 ) {
+			return dependency.substring( 0, index );
+		} else {
+			return dependency;
+		}
+    }
+
+	private Class<?>[] getParameterClasses( String[] parameters ) throws ClassNotFoundException {
+		Class<?>[] newParams = new Class<?>[parameters.length];
+		for ( int i = 0; i < parameters.length; i++ ) {
+			newParams[i] = Class.forName( parameters[i].trim() );
+		}
+		return newParams;
+	}
+
+	private String[] getParameters( String dependency ) {
+		int index;
+		if ( ( index = dependency.indexOf( "(" ) ) > -1 ) {
+			String params = dependency.substring( index + 1, dependency.length() - 1 );
+			return params.split( "," );
+		} else {
+			return new String[0];
+		}
+	}
+
+	private String[] getMethodNames() {
+		return this.annotationValue.split( ";" );
+	}
+
+}
