49bdbba4f4b4c0301280d3454d37c4caf2e4d578
Lea Haensenberger <baernerin@gmail.com>
2007-10-04 12:17:28 +0000

    git-svn-id: file:///Users/akuhn/Checkout/jeg/sf@75 54883e8d-cf1d-0410-83d6-f114b9419f37

--- trunk/src/experimental/tests/B.java
+++ trunk/src/experimental/tests/B.java
@@ -3,8 +3,13 @@ package experimental.tests;
 import org.junit.runner.RunWith;
 
 import experimental.FooRunner;
+import extension.annotations.MyTest;
 
 @RunWith(FooRunner.class)
 public class B {
 
+	@MyTest
+	public void otherTest(){
+		
+	}
 }
