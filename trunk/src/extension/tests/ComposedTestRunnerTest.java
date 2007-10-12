6cf6591af33e907f10897e9c4311d64e904a6ece
Lea Haensenberger <baernerin@gmail.com>
2007-10-12 09:00:46 +0000

    git-svn-id: file:///Users/akuhn/Checkout/jeg/sf@93 54883e8d-cf1d-0410-83d6-f114b9419f37

--- trunk/src/extension/tests/ComposedTestRunnerTest.java
+++ trunk/src/extension/tests/ComposedTestRunnerTest.java
@@ -6,7 +6,6 @@ package extension.tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
@@ -22,42 +21,6 @@ import extension.annotations.MyTest;
  */
 public class ComposedTestRunnerTest {
 
-	@RunWith(ComposedTestRunner.class)
-	static public class BadTestMethods {
-
-		public BadTestMethods() {
-		}
-
-		@MyTest
-		public void noParametersAccepted(String bla) {
-			assertTrue(false);
-		}
-
-		@MyTest
-		public boolean returnTypeNotVoid() {
-			assertTrue(false);
-			return true;
-		}
-
-		@MyTest
-		public void goodTest() {
-			assertTrue(true);
-		}
-	}
-
-	@Test
-	@Ignore
-	// the testmethods are not run, otherwise the test would fail
-	public void badTestMethods() {
-		Result result = JUnitCore.runClasses(BadTestMethods.class);
-		// if there are initialization errors, here, there are two of them, then
-		// for each
-		// initialization error, a ErrorReportingRunner is created and only
-		// those are run, so
-		// there are only two Runs
-		assertEquals(2, result.getRunCount());
-		assertEquals(2, result.getFailureCount());
-	}
 
 	@RunWith(ComposedTestRunner.class)
 	static public class CycleMethods {
