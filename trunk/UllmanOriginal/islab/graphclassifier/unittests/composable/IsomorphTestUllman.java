353f7e16dd7f302f8ad8ded9ae08d367cab8bad2
Lea Haensenberger <baernerin@gmail.com>
2007-11-09 10:00:18 +0000

    git-svn-id: file:///Users/akuhn/Checkout/jeg/sf@127 54883e8d-cf1d-0410-83d6-f114b9419f37

--- trunk/UllmanOriginal/islab/graphclassifier/unittests/composable/IsomorphTestUllman.java
+++ trunk/UllmanOriginal/islab/graphclassifier/unittests/composable/IsomorphTestUllman.java
@@ -1,4 +1,4 @@
-package islab.graphclassifier.unittests;
+package islab.graphclassifier.unittests.composable;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
