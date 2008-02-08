208a4ec5dc1cc29cfd92781916b3372b8286aa14
Lea Haensenberger <baernerin@gmail.com>
2008-02-08 12:44:29 +0000

    git-svn-id: file:///Users/akuhn/Checkout/jeg/sf@207 54883e8d-cf1d-0410-83d6-f114b9419f37

---
+++ JExample/trunk/src/demo/Demo.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Demo implements Cloneable{
+
+	private final String name;
+
+	public Demo(String name){
+		this.name = name;
+	}
+	
+	public boolean equals(Object obj){
+		return this.name.equals(((Demo)obj).name);
+	}
+	
+	public Object clone(){
+		return new Demo(this.name);
+	}
+}
