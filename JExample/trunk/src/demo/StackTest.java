df8bacfa1e0908e9a3641b182c73934fc2519a5e
Adrian Kuhn <akuhn@iam.unibe.ch>
2008-03-19 03:26:55 +0000

    First version of For.example()
    git-svn-id: file:///Users/akuhn/Checkout/jeg/sf@215 54883e8d-cf1d-0410-83d6-f114b9419f37

---
+++ JExample/trunk/src/demo/StackTest.java
@@ -0,0 +1,31 @@
+package demo;
+
+import static org.junit.Assert.*;
+
+import java.util.Stack;
+
+import jexample.Depends;
+import jexample.JExampleRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JExampleRunner.class)
+public class StackTest {
+
+	@Test
+	public Stack empty() {
+		Stack stack = new Stack();
+		assertEquals(0, stack.size());
+		return stack;
+	}
+	
+	@Test
+	@Depends("empty")
+	public Stack withValue(Stack stack) {
+		stack.push("boe");
+		assertEquals(1, stack.size());
+		return stack;
+	}
+
+}
---
+++ JExample/trunk/src/jexample/For.java
@@ -0,0 +1,47 @@
+package jexample;
+
+import org.junit.internal.runners.InitializationError;
+import org.junit.runner.notification.RunNotifier;
+
+import jexample.internal.*;
+
+/**
+ * Exercises test methods as sophisticated constructor to create example instances.
+ * 
+ *
+ */
+
+public class For {
+
+	private For() {
+		// TODO make non-static class
+	}
+	
+	public static <T> T example(Class test, String method) {
+		TestGraph graph = createTestGraph(test);
+		for (TestMethod each : graph.getTestMethods().values()) {
+			// TODO use dependency parser to find matching method
+			if (each.getDeclaringMethod().getName().equals(method)) {
+				RunNotifier notifier = new RunNotifier();
+				each.run(notifier);
+				// TODO check notifier for errors/assertions
+				return (T) each.getReturnValue();
+			}
+		}
+		// TODO verbose error message
+		throw new IllegalArgumentException("Method not found");
+	}
+
+	private static TestGraph createTestGraph(Class test) {
+		try {
+			TestGraph graph = new TestGraph();
+			TestClass testClass = new TestClass(test);
+			graph.addClass(testClass);
+			return graph;
+		} catch (InitializationError ex) {
+			// TODO verbose error message
+			throw new IllegalArgumentException(ex);
+		}
+	}
+	
+}
