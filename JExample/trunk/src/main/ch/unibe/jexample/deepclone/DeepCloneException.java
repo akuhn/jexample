873edd4ad1a88225d96a18c4e663caa5cde7078e
Lea Haensenberger <baernerin@gmail.com>
2009-06-17 09:17:22 +0000

    added deepclone classes
    git-svn-id: file:///Users/akuhn/Checkout/jeg/sf@292 54883e8d-cf1d-0410-83d6-f114b9419f37

---
+++ JExample/trunk/src/main/ch/unibe/jexample/deepclone/DeepCloneException.java
@@ -0,0 +1,11 @@
+package ch.unibe.jexample.deepclone;
+
+public class DeepCloneException extends RuntimeException {
+
+    private static final long serialVersionUID = 167984378038815842L;
+
+    public DeepCloneException(Throwable ex) {
+	super(ex);
+    }
+
+}
