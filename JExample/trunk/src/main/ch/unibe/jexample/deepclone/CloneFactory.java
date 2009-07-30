f3a91c959364fc72786bdd9fa1b0f6add2da2a3c
Adrian Kuhn <akuhn@iam.unibe.ch>
2009-07-30 14:35:53 +0000

    Hidden developer flag, now possible to debug trace of clones.
    git-svn-id: file:///Users/akuhn/Checkout/jeg/sf@335 54883e8d-cf1d-0410-83d6-f114b9419f37

--- JExample/trunk/src/main/ch/unibe/jexample/deepclone/CloneFactory.java
+++ JExample/trunk/src/main/ch/unibe/jexample/deepclone/CloneFactory.java
@@ -2,6 +2,7 @@ package ch.unibe.jexample.deepclone;
 
 import java.util.IdentityHashMap;
 import java.util.Map;
+import java.util.Stack;
 
 /** Deep-clones any object. Uses unsafe reflection to clone any objects, no matter whether cloneable or not.
  * Does not call any methods, neither constructors nor accessors nor <code>clone</code> methods.
@@ -14,6 +15,9 @@ import java.util.Map;
  */
 public class CloneFactory {
 
+	public static boolean DEBUG_TRACE = false;
+	private static ThreadLocal<Stack<String>> debugTraceStack = new ThreadLocal<Stack<String>>();
+	
 	private Map<Object,Object> done = new IdentityHashMap<Object,Object>();
 	private DeepCloneStrategyCache cache = DeepCloneStrategyCache.getDefault();
 
@@ -21,12 +25,25 @@ public class CloneFactory {
 	public <T> T clone(T original) throws DeepCloneException {
 		try {
 			if (original == null) return null;
-			return (T) cache.lookup(original).makeClone(original, this);
+			debugPush(original);
+			T clone = (T) cache.lookup(original).makeClone(original, this);
+			debugPop();
+			return clone;
 		} catch (DeepCloneException ex) {
 			throw ex;
 		} catch (Throwable ex) {
-			throw new DeepCloneException(ex);
+			throw new DeepCloneException(debugTraceStack, ex);
 		}
+	}
+
+	private void debugPush(Object original) {
+		if (debugTraceStack.get() == null) debugTraceStack.set(new Stack<String>());
+		debugTraceStack.get().push(original.getClass().toString());
+	}
+	
+	private void debugPop() {
+		if (debugTraceStack.get() == null) debugTraceStack.set(new Stack<String>());
+		debugTraceStack.get().pop();
 	}
 
 	public static <T> T deepClone(T object) {
--- JExample/trunk/src/main/ch/unibe/jexample/deepclone/DeepCloneException.java
+++ JExample/trunk/src/main/ch/unibe/jexample/deepclone/DeepCloneException.java
@@ -1,11 +1,19 @@
 package ch.unibe.jexample.deepclone;
 
+import java.util.Stack;
+
 public class DeepCloneException extends RuntimeException {
 
-    private static final long serialVersionUID = 167984378038815842L;
+	private static final long serialVersionUID = 167984378038815842L;
 
-    public DeepCloneException(Throwable ex) {
-	super(ex);
-    }
+	public DeepCloneException(ThreadLocal<Stack<String>> debugTraceStack, Throwable ex) {
+		super(toDebugString(debugTraceStack), ex);
+	}
+
+	private static String toDebugString(ThreadLocal<Stack<String>> debugTraceStack) {
+		String debug = "";
+		for (String each: debugTraceStack.get()) debug += each + " in " + debug;
+		return debug;
+	}
 
 }
